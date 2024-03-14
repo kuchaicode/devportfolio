c45fed9edc47fc0ff13943ecf632a6a9456d10be
kuchaidev <kristntan@gmail.com>
2024-03-14 21:35:24 +0800

    Add header button styles and animation

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import Image from 'next/image'
+import Link from 'next/link';
 import React from 'react'
 import { motion } from 'framer-motion';
+import { LuArrowRight, LuDownload, LuGithub, LuLinkedin } from 'react-icons/lu';
 
 export default function Intro() {
   return (
-    <section>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
         <div className='flex items-center justify-center'>
             <div className='relative'>
                 <motion.div
@@ -42,10 +44,54 @@ export default function Intro() {
                 </motion.span>
             </div>
         </div>
-        {/* Portrait end */}
-        <p>
-            
-        </p>
+        {/* Portrait end */}        
+        <motion.p 
+            className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+            initial={{ opacity: 0, y: 100}}
+            animate={{ opacity: 1, y: 0}}
+            >
+            <span className="font-bold">Hello, I'm Kristen!</span> I'm a{" "}
+            <span className="font-bold">full-stack developer. </span> I enjoy
+            building responsive <span className="italic">sites & apps</span> with beautiful <span className='font-bold'>UI</span> and proper <span className='font-bold'>UX.</span>{" "} My focus right now is{" "}
+            <span className="underline">React (Next.js)</span>.
+        </motion.p>
+        {/* Intro text end */}
+        <motion.div 
+            className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 font-medium text-lg'
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+                delay: 0.2,
+            }}
+            >
+            <Link 
+            href='#contact'
+            className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'
+            >
+                Contact me here! <LuArrowRight className='opacity-65 group-hover:translate-x-1 transition'
+                />
+            </Link>
+            <a className='group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition border border-black/10 cursor-pointer' href='/KT_CV.pdf' download={true}>
+                Download CV 
+                <LuDownload 
+            className='opacity-60 group-hover:translate-y-1 transition'
+            />
+            </a>
+            <a 
+            className='group bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 cursor-pointer'
+            href='https://linkedin.com'
+            target="_blank"
+            >
+                <LuLinkedin />
+            </a>
+            <a className='group bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border border-black/10 cursor-pointer'
+            href='https://github.com'
+            target="_blank"
+            >
+                <LuGithub />
+            </a>
+        </motion.div>
+        {/* Buttons section end */}
     </section>
   )
 }
