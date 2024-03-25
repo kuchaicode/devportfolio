b87e05a6a8f7602b603084f192ff57534c955d8d
kuchaidev <kristntan@gmail.com>
2024-03-25 15:06:45 +0800

    Add card hover animations

--- components/projects.tsx
+++ components/projects.tsx
@@ -26,7 +26,7 @@ function Project({
 }: ProjectProps) 
 {
     return (
-        <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8'>
+        <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
@@ -42,7 +42,15 @@ function Project({
             src={imageUrl} 
             alt='Project that I worked on.' 
             quality={95} 
-            className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40'/>
+            className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl 
+            group-even:right-[initial] 
+            group-even:-left-40 
+            group-hover:-translate-x-2 group-hover:translate-y-3 
+            group-hover:-rotate-2 
+            group-hover:scale-[1.06] 
+            group-even:group-hover:translate-x-2 group-even:group-hover:translate-y-3 
+            group-even:group-hover:rotate-2 
+            transition'/>
 
         </section>
     );
