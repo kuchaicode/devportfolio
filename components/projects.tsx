3e86bd9c365e545f4e48151426934a96865ccd70
kuchaidev <kristntan@gmail.com>
2024-03-25 14:43:44 +0800

    Fix card alignment

--- components/projects.tsx
+++ components/projects.tsx
@@ -26,8 +26,8 @@ function Project({
 }: ProjectProps) 
 {
     return (
-        <section className='bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8'>
-            <div className='pt-4 pb-8 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full'>
+        <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8'>
+            <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
                 <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
@@ -38,7 +38,11 @@ function Project({
                     ))}
                 </ul>
             </div>
-            <Image src={imageUrl} alt='Project that I worked on.' quality={95} className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl'/>
+            <Image 
+            src={imageUrl} 
+            alt='Project that I worked on.' 
+            quality={95} 
+            className='absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40'/>
 
         </section>
     );
