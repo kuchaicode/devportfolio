e70b3afc7eb14eaa6cd7231edb9006b76d59ae6b
kuchaidev <kristntan@gmail.com>
2024-03-26 17:03:34 +0800

    Add framer motion to projects cards

--- components/project.tsx
+++ components/project.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import { useRef } from "react";
-import { useScroll } from "framer-motion";
+import { useScroll, useTransform } from "framer-motion";
 import Image from "next/image";
 import { projectsData } from "@/lib/data";
+import { motion } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -11,15 +12,28 @@ export default function Project({
     title, description, tags, imageUrl
 }: ProjectProps) 
 {
-    const ref = useRef(null);
+    const ref = useRef<HTMLDivElement>(null);
 
-    useScroll({
-        target: '',
+    const { scrollYProgress } = useScroll({
+        target: ref,
         offset: ["0 1", "1.33 1"],
     });
+
+    const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.7, 1]);
+    const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
     // End of Framer scroll effect
     return (
-        <section className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition'>
+        <motion.div
+        ref={ref} 
+        style={{
+            scale: scaleProgress,
+            opacity: opacityProgress,
+        }}
+        className="mb-3 sm:mb-8 last:mb-0"
+        >
+             <section 
+            className='group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  even:pl-8 hover:bg-gray-200 transition'>
             <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
                 <h3 className='text-2xl font-semibold'>{title}</h3>
                 <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
@@ -47,5 +61,7 @@ export default function Project({
             transition'/>
 
         </section>
+        </motion.div>
+       
     );
 }
