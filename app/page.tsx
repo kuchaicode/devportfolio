cb8a45bbad45103bcc33594b2ebb4773be68e0aa
kuchaidev <kristntan@gmail.com>
2024-04-19 16:01:17 +0800

    Add skills section with framer motion animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Projects from "@/components/projects";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
